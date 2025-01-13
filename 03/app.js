f569e9148863c0a882d2ad634d274e7886b89ab6
Bartosz Dudziak <bartoszdudziak.dev@gmail.com>
2025-01-13 08:33:02 +0100

    Task 03 - Test 2 - blue

--- 03/app.js
+++ 03/app.js
@@ -1,11 +1,15 @@
 export default function randomNumber(min, max) {
-  if (typeof min !== "number") {
+  if (!isNumber(min)) {
     throw new TypeError("Min must be a number");
   }
 
-  if (typeof max !== "number") {
+  if (!isNumber(max)) {
     throw new TypeError("Min must be a number");
   }
 
   return Math.floor(Math.random() * (min - max) + max);
+}
+
+function isNumber(value) {
+  return typeof value === "number" && !Number.isNaN(value);
 }
