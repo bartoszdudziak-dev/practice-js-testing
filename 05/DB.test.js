74945cc4eee6594efaf4932d8de07d7f2bf48470
Bartosz Dudziak <bartoszdudziak.dev@gmail.com>
2025-01-14 00:55:57 +0100

    Task 05 - Select and clear items - tests

--- 05/DB.test.js
+++ 05/DB.test.js
@@ -1,30 +1,7 @@
 import DB from "./DB";
 
 describe("Database", () => {
-  describe("Insert", () => {
-    it("resolves and creates new record when data is valid", async () => {
-      const database = new DB();
-      const data = { id: 1, a: 10 };
-
-      await expect(database.insert(data)).resolves.toStrictEqual(data);
-      expect(database._rows).toContainEqual(data);
-    });
-
-    it("resolves and creates new record when id is not provided", async () => {
-      const database = new DB();
-      database._rows = [{ id: 1, a: 10 }];
-
-      const data = { a: 20 };
-      await expect(database.insert(data)).resolves.toStrictEqual({
-        id: 2,
-        a: 20,
-      });
-      expect(database._rows).toContainEqual({
-        id: 2,
-        a: 20,
-      });
-    });
-
+  describe("Insert item", () => {
     it("rejects when given id is not a number", async () => {
       const database = new DB();
       const data = { id: "ID", a: 10 };
@@ -45,9 +22,32 @@ describe("Database", () => {
       );
       expect(database._rows).toEqual([{ id: 1, a: 10 }]);
     });
+
+    it("resolves and creates new record when id is valid", async () => {
+      const database = new DB();
+      const data = { id: 1, a: 10 };
+
+      await expect(database.insert(data)).resolves.toStrictEqual(data);
+      expect(database._rows).toContainEqual(data);
+    });
+
+    it("resolves and creates new record with unique id when id is not provided", async () => {
+      const database = new DB();
+      database._rows = [{ id: 1, a: 10 }];
+
+      const data = { a: 20 };
+      await expect(database.insert(data)).resolves.toStrictEqual({
+        id: 2,
+        a: 20,
+      });
+      expect(database._rows).toContainEqual({
+        id: 2,
+        a: 20,
+      });
+    });
   });
 
-  describe("Remove", () => {
+  describe("Remove item", () => {
     it("resolves and removes item", async () => {
       const database = new DB();
       database._rows = [
@@ -71,6 +71,79 @@ describe("Database", () => {
         { id: 1, a: 10 },
         { id: 2, a: 20 },
       ]);
+    });
+  });
+
+  describe("Update item", () => {
+    it("rejects when id is not provided", async () => {
+      const database = new DB();
+      database._rows = [{ id: 1, a: 10 }];
+      const data = { a: 20 };
+
+      await expect(database.update(data)).rejects.toEqual("ID have to be set!");
+      expect(database._rows).toEqual([{ id: 1, a: 10 }]);
+    });
+
+    it("rejects when id is not found", async () => {
+      const database = new DB();
+      database._rows = [{ id: 1, a: 10 }];
+      const data = { id: 2, a: 20 };
+
+      await expect(database.update(data)).rejects.toEqual("ID not found!");
+      expect(database._rows).toEqual([{ id: 1, a: 10 }]);
+    });
+
+    it("resovles and updates item when id is found", async () => {
+      const database = new DB();
+      database._rows = [{ id: 1, a: 10 }];
+      const data = { id: 1, a: 20 };
+
+      await expect(database.update(data)).resolves.toStrictEqual(data);
+      expect(database._rows).toEqual([{ id: 1, a: 20 }]);
+    });
+  });
+
+  describe("Get items", () => {
+    it("resolves and returns all items", async () => {
+      const database = new DB();
+      database._rows = [
+        { id: 1, a: 10 },
+        { id: 2, a: 20 },
+      ];
+
+      await expect(database.getRows()).resolves.toStrictEqual([
+        { id: 1, a: 10 },
+        { id: 2, a: 20 },
+      ]);
+    });
+  });
+
+  describe("Get specify item", () => {
+    it("rejects when id is not found", async () => {
+      const database = new DB();
+      database._rows = [{ id: 1, a: 10 }];
+
+      await expect(database.select(2)).rejects.toEqual("ID not found");
+    });
+
+    it("resolves and returns when item exists", async () => {
+      const database = new DB();
+      database._rows = [{ id: 1, a: 10 }];
+
+      await expect(database.select(1)).resolves.toStrictEqual({ id: 1, a: 10 });
+    });
+  });
+
+  describe("Clear items", () => {
+    it("resolves and clear database", async () => {
+      const database = new DB();
+      database._rows = [
+        { id: 1, a: 10 },
+        { id: 2, a: 20 },
+      ];
+
+      await expect(database.truncate()).resolves.toEqual(true);
+      expect(database._rows).toHaveLength(0);
     });
   });
 });
