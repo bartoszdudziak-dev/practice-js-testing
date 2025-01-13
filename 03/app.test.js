ee238a8a9f18cdadfdd2c35576d407009a82fcd8
Bartosz Dudziak <bartoszdudziak.dev@gmail.com>
2025-01-13 08:00:06 +0100

    Task 03 - Test 1 - red

--- 03/app.test.js
+++ 03/app.test.js
@@ -1,1 +1,8 @@
-import randomNumber from './app';
+import randomNumber from "./app";
+
+describe("Random number generator", () => {
+  it("returns 1 when min and max are set to 1", () => {
+    const number = randomNumber(1, 1);
+    expect(number).toBe(1);
+  });
+});
