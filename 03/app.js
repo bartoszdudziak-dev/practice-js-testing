15735bbf496ca7830f4e22b834e3cf534452a5bb
Bartosz Dudziak <bartoszdudziak.dev@gmail.com>
2025-01-13 08:26:51 +0100

    Task 03 - Test 2 - green

--- 03/app.js
+++ 03/app.js
@@ -1,3 +1,11 @@
 export default function randomNumber(min, max) {
+  if (typeof min !== "number") {
+    throw new TypeError("Min must be a number");
+  }
+
+  if (typeof max !== "number") {
+    throw new TypeError("Min must be a number");
+  }
+
   return Math.floor(Math.random() * (min - max) + max);
 }
